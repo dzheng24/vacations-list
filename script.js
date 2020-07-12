e585f4033b978a14ad298921fb076e0be699f7bd
David Zheng <zhengdav@msu.edu>
2020-07-12 08:52:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// for each of the input field, create a variable that holds the input
+
+document
+  .getElementById("inputDestinationName")
+  .addEventListener("change", handleDestinationName);
+
+document
+  .getElementById("inputLocation")
+  .addEventListener("change", handleLocation);
+
+document.getElementById("inputPhoto").addEventListener("change", handlePhoto);
+
+document
+  .getElementById("inputDescription")
+  .addEventListener("change", handleDescription);
+
+function handleDestinationName() {
+  let data = document.getElementById("inputDestinationName").value;
+  console.log(data);
+}
+
+function handleLocation() {
+  let data = document.getElementById("inputLocation").value;
+  console.log(data);
+}
+
+function handlePhoto() {
+  let data = document.getElementById("inputPhoto").value;
+  console.log(data);
+}
+
+function handleDescription() {
+  let data = document.getElementById("inputDescription").value;
+  console.log(data);
+}
+
+// for the click button, create function that when clicked:
+// generates a card
+// append the information from the input fields to the card
+document.getElementById("my-button").addEventListener("click", generateCard);
+
+function generateCard(e) {
+  e.preventDefault();
+  console.log("clicked");
+  // let test = document.getElementById("my-image");
+  // test.src = "./test.jpeg";
+  // let newPartial = document.getElementById("card-div").append(test);
+  // document.getElementById("display-area").append(newPartial);
+}
